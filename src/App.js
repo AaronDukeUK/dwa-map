f9d7ea85f6de8db8b81acd25a735e1b6f9fdb753
Aaron Duke <hello@aaronduke.co.uk>
2022-11-09 18:23:46 +0000

    map

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import "./App.css"
 
 function App() {
-  return <div className="App"></div>
+  return (
+    <div className="App">
+      <iframe
+        src="https://www.google.com/maps/d/embed?mid=1Kp6aN-AEGMvm8HS2qQrcCfxlfI7dkss&ehbc=2E312F"
+        width="640"
+        height="480"
+      ></iframe>
+    </div>
+  )
 }
 
 export default App
